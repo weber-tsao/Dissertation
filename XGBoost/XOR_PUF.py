e52cb7b2bb96b35f44b92dbccf0b35c8fa6cecd9
weber-tsao <67998511+weber-tsao@users.noreply.github.com>
2022-05-09 07:24:29 +0100

    polish code and delete some code

--- XGBoost/XOR_PUF.py
+++ XGBoost/XOR_PUF.py
@@ -45,12 +45,12 @@ class XOR_PUF:
         puf4 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=67)
         puf5 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=90)
         puf6 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=111)'''
-        puf1 = pypuf.simulation.ArbiterPUF(n=(stages), seed=puf_seed1)
-        puf2 = pypuf.simulation.ArbiterPUF(n=(stages), seed=puf_seed2)
-        puf3 = pypuf.simulation.ArbiterPUF(n=(stages), seed=puf_seed3)
-        puf4 = pypuf.simulation.ArbiterPUF(n=(stages), seed=puf_seed4)
-        puf5 = pypuf.simulation.ArbiterPUF(n=(stages), seed=puf_seed5)
-        puf6 = pypuf.simulation.ArbiterPUF(n=(stages), seed=puf_seed6)
+        puf1 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=puf_seed1)
+        puf2 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=puf_seed2)
+        puf3 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=puf_seed3)
+        puf4 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=puf_seed4)
+        puf5 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=puf_seed5)
+        puf6 = pypuf.simulation.ArbiterPUF(n=(stages-4), seed=puf_seed6)
         puf_list = [puf1, puf2, puf3, puf4, puf5, puf6]
         #puf = XORArbiterPUF(n=(stages-4), k=xor_num, seed=21)
         lfsrChallenges = random_inputs(n=stages, N=data_num, seed=cus_seed) # LFSR random challenges data
@@ -70,13 +70,13 @@ class XOR_PUF:
             challenge = test_crps[i]
             
             # obfuscate part
-            #obfuscateChallenge = self.LFSR_simulated.createObfuscateChallenge(challenge, base)
-            #obfuscateChallenge = [-1 if c == 0 else c for c in obfuscateChallenge]
+            obfuscateChallenge = self.LFSR_simulated.createObfuscateChallenge(challenge, base)
+            obfuscateChallenge = [-1 if c == 0 else c for c in obfuscateChallenge]
             data_r = 0
             for p in range(xor_num):
-                obfuscateChallenge = self.Puf_resilience.cyclic_shift(challenge, puf_list[p])
-                obfuscateChallenge = [-1 if c == 0 else c for c in obfuscateChallenge]
-                final_delay_diffc = self.total_delay_diff(challenge, puf_list[p])
+                #obfuscateChallenge = self.Puf_resilience.cyclic_shift(challenge, puf_list[p])
+                #obfuscateChallenge = [-1 if c == 0 else c for c in obfuscateChallenge]
+                final_delay_diffc = self.total_delay_diff(challenge[4:], puf_list[p])
                 
                 #final_delay_diffc = puf_list[p].val(np.array([obfuscateChallenge]))
                 
@@ -95,7 +95,7 @@ class XOR_PUF:
                     
                     data_r = data_r^response1
                     
-            #challenge = challenge[4:]
+            challenge = challenge[4:]
             challenge = [0 if c == -1 else c for c in challenge]
                                                     
             data.append(challenge)
